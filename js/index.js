33cce04f77adf7ecb733e86f5b2b96a47086dc3a
Daniel <daniel-ontiveros@lambdastudents.com>
2020-08-08 00:45:52 -0700

    2 star!

--- js/index.js
+++ js/index.js
@@ -44,8 +44,98 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 let ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute('src',siteContent["cta"]["img-src"])
 
-// let ctaText = document.getElementsByClassName("cta-text h1")
-// ctaText.textContent(siteContent["cta"]["h1"])
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src',siteContent["main-content"]["middle-img-src"])
+
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.textContent = siteContent["cta"]["h1"];
+
+let ctaBtn = document.querySelector(".cta-text button");
+ctaBtn.textContent = siteContent["cta"]["button"];
+
+let textH4 = document.querySelectorAll(".text-content h4");
+textH4[0].textContent = siteContent["main-content"]["features-h4"];
+textH4[1].textContent = siteContent["main-content"]["about-h4"];
+textH4[2].textContent = siteContent["main-content"]["services-h4"];
+textH4[3].textContent = siteContent["main-content"]["product-h4"];
+textH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let textP = document.querySelectorAll(".text-content p");
+textP[0].textContent = siteContent["main-content"]["features-content"];
+textP[1].textContent = siteContent["main-content"]["about-content"];
+textP[2].textContent = siteContent["main-content"]["services-content"];
+textP[3].textContent = siteContent["main-content"]["product-content"];
+textP[4].textContent = siteContent["main-content"]["vision-content"];
+
+let contactH4 = document.querySelector(".contact h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactP = document.querySelectorAll(".contact p");
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
+let footerP = document.querySelector("footer p");
+footerP.textContent = siteContent["footer"]["copyright"];
+
+let nav = document.querySelectorAll("nav a");
+nav.forEach(function(value, i){
+  value.textContent = siteContent.nav[`nav-item-${i+1}`];
+});
+
+let navNew = document.createElement('a');
+let navNew1 = document.createElement('a');
+let navANew = document.querySelector('nav')
+navNew.textContent = "Order";
+navNew1.textContent = "Order Now";
+navANew.prepend(navNew);
+navANew.append(navNew1);
+
+nav.forEach(value => { 
+  value.style.color= '#B0FF92';
+});
+navNew.style.color= '#B0FF92';
+navNew1.style.color= '#B0FF92';
+
+
+
+// const nav = document.querySelectorAll("header nav a")
+// nav.forEach(function(value, i){
+//   value.textContent = siteContent.nav[`nav-item-${i+1}`]
+// nav.forEach(value => { 
+//   value.style.color= '#B0FF92';
+// })
+// });
+
+
+// const newContent1 = document.createElement('a');
+// newContent1.textContent = "Order";
+// const newA1 = document.querySelector('nav');
+// newA1.prepend(newContent1);
+
+// const newContent = document.createElement('a');
+// newContent.textContent = "Order Now";
+// const newA = document.querySelector('nav');
+// newA.append(newContent);
+
+
+
+// let topTextP = document.querySelector("p");
+// topTextP.textContent = siteContent["main-content"]["features-content"];
+
+// let topContH4 = document.querySelectorAll("h4.top-content")
+// topContH4.forEach(function(value, i){
+//   value.textContent = siteContent.main-content[`${i+1}-h4`]
+// });
+// console.log(topContH4)
+
+
+
+// let ctaText = document.getElementsByClassName("cta-text").h1;
+// ctaText.textContext = siteContent["cta"]["h1"];
+
+// let ctaText = document.getElementsByClassName("cta-text").h1;
+// ctaText.textContext(siteContent["cta"]["h1"]);
 
 // let ctaText = document.getElementsByClassName("cta-text h1")
 // ctaText.childNodes[1].textContent(siteContent["cta"]["h1"])
@@ -59,28 +149,6 @@ ctaImg.setAttribute('src',siteContent["cta"]["img-src"])
 
 
 
-
-
-
-
-
-const nav = document.querySelectorAll("header nav a")
-nav.forEach(function(value, i){
-  value.textContent = siteContent.nav[`nav-item-${i+1}`]
-nav.forEach(value => { 
-  value.style.color= '#B0FF92';
-})
-});
-
-const newContent1 = document.createElement('a');
-newContent1.textContent = "Order";
-const newA1 = document.querySelector('nav');
-newA1.prepend(newContent1);
-
-const newContent = document.createElement('a');
-newContent.textContent = "Order Now";
-const newA = document.querySelector('nav');
-newA.append(newContent);
 
 
 
